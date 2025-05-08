1c7e4c74184e5adbcef2f3a7c3b4cb939c1e35ef
jazmy <jazmy@JAZ-LAPTOP>
2025-05-08 16:19:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿static bool CanPlantFlower(int[] FlowerBed, int n)
+{
+    for (int i = 0; i < FlowerBed.Length && n > 0; i++)
+    {
+        if (FlowerBed[i] == 0)
+        {
+            bool LeftEmpty = (i == 0) || (FlowerBed[i - 1] == 0);
+            bool RightEmpty = (i == 0) || (FlowerBed[i + 1] == 0);
+
+            if (LeftEmpty && RightEmpty)
+            {
+                FlowerBed[i] = 1;
+                n--;
+                i++;
+            }
+            
+        }
+    }
+    return n == 0;
+}
+
+int[] FlowerBed = { 1, 0, 0, 0, 1 };
+
+Console.Write("Please input the number of flowers you would like to plant: ");
+int n = Convert.ToInt32(Console.ReadLine());
+bool result = CanPlantFlower(FlowerBed, n);
+Console.WriteLine($"Can you plant {n} flower(s)? {result}");
+
+
+
+
+
+
+
